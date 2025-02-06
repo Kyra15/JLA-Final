8a4f88b93dbaf4129c69b96119652302198a52ec
Daniel C <144239595+DanielC8@users.noreply.github.com>
2025-02-06 13:59:47 -0500

    DONE!!

--- static/js/login.js
+++ static/js/login.js
@@ -1,4 +1,6 @@
+// add a click event for the eye icon
 document.getElementById('eye-icon').addEventListener('click', function() {
+    //toggles type of the password
     let passwordIn = document.getElementById('password');
     if (passwordIn.type === 'password') {
         passwordIn.type = 'text';
@@ -8,7 +10,7 @@ document.getElementById('eye-icon').addEventListener('click', function() {
         this.classList.replace("fa-eye-slash", "fa-eye");
     }
 });
-
+//checks login form
 async function validateForm() {
     event.preventDefault();
 
@@ -17,13 +19,14 @@ async function validateForm() {
     console.log(username, password);
 
     const user_data = await fetchFromDB(username);
-
+    //checks the username and password
     if (user_data && user_data.password === password) {
         current_user = user_data;
         localStorage.setItem('current_user', JSON.stringify(current_user));
         changePage('home');
         return true;
     } else {
+        //if password check fails 
         window.alert("Invalid username or password");
         document.getElementById("username").value = "";
         document.getElementById("password").value = "";
@@ -31,7 +34,7 @@ async function validateForm() {
     }
 }
 
-
+//fetches from database with username
 async function fetchFromDB(username) {
     try {
         const response = await fetch('/accounts_testbank');
