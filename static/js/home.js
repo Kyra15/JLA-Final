f27cea66337a08ffb55453f98c6360ea8db22225
Daniel C <144239595+DanielC8@users.noreply.github.com>
2025-02-06 13:55:19 -0500

    Updated

--- static/js/home.js
+++ static/js/home.js
@@ -1,6 +1,10 @@
+// select all elements with the class 'period' from the document
 document.querySelectorAll('.period').forEach(function(period) {
+    // add a click event listener to each selected element
     period.addEventListener('click', function() {
+        // get the value of the 'data-url' attribute from the clicked element
         const url = period.getAttribute('data-url');
+        // changes page to the respective forum
         changePage(url)
     });
 });
